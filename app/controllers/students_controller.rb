4425922fb4a738ff3d1cd1947f5533f1ade8098b
Ibaihima <ibaihima@hotmail.com>
2022-04-03 16:28:14 -0400

    commit tree 63896f67b42bfe9ff77fbeefd51b80854f0bad9a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,6 +3,12 @@ class StudentsController < ApplicationController
   def index
     students = Student.all
     render json: students
+    
+  end
+
+  def show_by_id
+    id = Student.find_by(id: params[:id])
+    render json: id
   end
 
 end
